d9b6cd2ad8c45e63f06a25da502031f67302ac5e
Michael <michaelhsusanto@gmail.com>
2026-07-02 13:27:12 +0700

    Show full url for product download

--- frontend/app/components/product/ProductDownloadWindow.vue
+++ frontend/app/components/product/ProductDownloadWindow.vue
@@ -10,7 +10,7 @@
   <v-card-text>
     <h3 v-if="isFetching">Product is downloading...</h3>
     <div v-else-if="data">
-      <h3>Product can be open in: {{ data.downloadProducts }}</h3>
+      <h3>Product can be open in: {{ fullUrl }}</h3>
       <v-btn color="primary" @click="downloadProduct"> Download </v-btn>
     </div>
   </v-card-text>
@@ -23,17 +23,21 @@ import { DownloadProductsDocument } from '~/api/generated/types';
 const { data, isFetching, error } = useQuery({
   query: DownloadProductsDocument,
   fetchOnMount: true,
+  onData(data) {
+    const config = useRuntimeConfig();
+    const routerBase = config.public.baseUrl.replace('/graphql', '');
+
+    const url = data?.downloadProducts;
+    if (!url) return;
+
+    fullUrl.value = `${routerBase}${url}`;
+  },
 });
 
+const fullUrl = ref('');
+
 const downloadProduct = async () => {
-  const config = useRuntimeConfig();
-  const routerBase = config.public.baseUrl.replace('/graphql', '');
-
-  const url = data?.value?.downloadProducts;
-  if (!url) return;
-
-  const fullUrl = `${routerBase}${url}`;
-  const res = await fetch(fullUrl);
+  const res = await fetch(fullUrl.value);
   console.log(fullUrl);
   if (!res.ok) throw new Error(`Download failed: ${res.status}`);
 
